57e5f0745dd8a7efecedb5c3b70385e031f8f844
HuanNguyen454 <nhuan454@gmail.com>
2025-07-13 23:33:18 +0700

    Huan add Flag function

---
+++ ViewModels/UserVerificationViewModel.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace StudentFreelance.ViewModels
+{
+    public class UserVerificationViewModel
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public bool IsVerified { get; set; }
+        public bool IsFlagged { get; set; }
+        public string FlagReason { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+        public int StatusID { get; set; }
+    }
+} 
