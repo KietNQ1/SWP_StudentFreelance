9e01e61ee65413ae99ae715f02ce8b054357670f
huy <ngochuy10012003@gmail.com>
2025-07-27 15:56:48 +0700

    huy update chatting

---
+++ ViewModels/ChatPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using StudentFreelance.Models;
+
+namespace StudentFreelance.ViewModels
+{
+    public class ChatPageViewModel
+    {
+        public List<ConversationDto> Conversations { get; set; }
+        public List<Project> Projects { get; set; }
+        public int? SelectedProjectID { get; set; }
+        public ChatRoomViewModel? ChatRoom { get; set; }
+    }
+}
